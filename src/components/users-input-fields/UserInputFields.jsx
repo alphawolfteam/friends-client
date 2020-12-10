3f9ffc2338772034e36fa716533d2d129cc926d7
CarmelTsadok <carmelcarmel88888@gmail.com>
2020-12-10 16:27:46 +0200

    feat(UserInputFields.jsx): Add getUserIndex function

--- src/components/users-input-fields/UserInputFields.jsx
+++ src/components/users-input-fields/UserInputFields.jsx
@@ -9,6 +9,7 @@ import { ReactComponent as RemoveManagerIcon } from '../../images/removeManagerI
 import { ReactComponent as AddManagerIcon } from '../../images/addManagerIcon.svg';
 import UserSearchBar from '../user-search-bar/UserSearchBar';
 import GroupService from '../../services/GroupsService';
+import UsersService from '../../services/UsersService';
 
 const { rolesEnum } = config;
 
@@ -16,9 +17,26 @@ const isExist = (usersList, userToCheck) => {
   return usersList.map((user) => user.id).includes(userToCheck.id);
 };
 
+const getUserIndex = (usersList, userToFind) => {
+  return usersList.map((user) => user.id).indexOf(userToFind.id);
+};
+
 const UserInputFields = ({ group, setGroup }) => {
   const classes = useStyles();
   const [selectedUser, setSelectedUser] = useState(undefined);
+  const [populatedUsers, setPopulatedUsers] = useState([]);
+
+  // TODO: On changeing page
+  useEffect(async () => {
+    // TODO: Populate first 5 users
+    setPopulatedUsers(
+      await UsersService.getPopulatedUsersList(group.users.map((user) => user.id)),
+    );
+  }, [group]);
+
+  // TODO: Sort the users- managers at the top and bold
+  // TODO: (?) Add useEffect that when changing someone from and to be a manager
+  // its reset the pages to 1 and populates the users again
 
   useEffect(() => {
     if (selectedUser && !isExist(group.users, selectedUser)) {
@@ -26,64 +44,65 @@ const UserInputFields = ({ group, setGroup }) => {
         ...prevValue,
         users: [
           ...prevValue.users,
-          { ...selectedUser, role: rolesEnum.FRIEND },
+          { id: selectedUser.id, role: rolesEnum.FRIEND },
         ],
       }));
     }
   }, [setGroup, selectedUser]);
 
-  const handleRemoveUser = (index) => {
+  const handleRemoveUser = (userToRemove) => {
     setGroup((prevValue) => {
       const usersList = [...prevValue.users];
-      usersList.splice(index, 1);
+      usersList.splice(getUserIndex(usersList, userToRemove), 1);
       return { ...prevValue, users: usersList };
     });
   };
 
-  const handleChangeRole = (index, newRole) => {
+  const handleChangeRole = (userToChange, newRole) => {
     setGroup((prevValue) => {
       const usersList = [...prevValue.users];
-      usersList[index].role = newRole;
+      usersList[getUserIndex(usersList, userToChange)].role = newRole;
       return { ...prevValue, users: usersList };
     });
   };
 
   const isAManager = (userId) => GroupService.isAManager(group, userId);
 
+  const managerButton = (user) => (isAManager(user.id) ? (
+    <Tooltip title="הסרה ממנהל">
+      <RemoveManagerIcon
+        className={classes.iconButton}
+        onClick={() => handleChangeRole(user, rolesEnum.FRIEND)}
+      />
+    </Tooltip>
+  ) : (
+    <Tooltip title="הוספה למנהל">
+      <AddManagerIcon
+        className={classes.iconButton}
+        onClick={() => handleChangeRole(user, rolesEnum.MANAGER)}
+      />
+    </Tooltip>
+  ));
+
   return (
     <>
       <UserSearchBar setSelectedUser={setSelectedUser} />
       <div className={classes.fieldList}>
         <Scrollbar>
-          {group.users
-            && group.users.map((user, index) => (
-              <div key={user.id} className={classes.field}>
-                <Tooltip title="מחיקה">
-                  <Button
-                    className={classes.iconButton}
-                    onClick={() => handleRemoveUser(index)}
-                  >
-                    <Delete />
-                  </Button>
-                </Tooltip>
-                {isAManager(user.id) ? (
-                  <Tooltip title="הסרה ממנהל">
-                    <RemoveManagerIcon
-                      className={classes.iconButton}
-                      onClick={() => handleChangeRole(index, rolesEnum.FRIEND)}
-                    />
-                  </Tooltip>
-                ) : (
-                  <Tooltip title="הוספה למנהל">
-                    <AddManagerIcon
-                      className={classes.iconButton}
-                      onClick={() => handleChangeRole(index, rolesEnum.MANAGER)}
-                    />
-                  </Tooltip>
-                )}
-                <UserRaw user={user} />
-              </div>
-            ))}
+          {populatedUsers.map((user) => (
+            <div key={user.id} className={classes.field}>
+              <Tooltip title="מחיקה">
+                <Button
+                  className={classes.iconButton}
+                  onClick={() => handleRemoveUser(user)}
+                >
+                  <Delete />
+                </Button>
+              </Tooltip>
+              {managerButton(user)}
+              <UserRaw user={user} />
+            </div>
+          ))}
         </Scrollbar>
       </div>
     </>

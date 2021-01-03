91b542e663b043fd1ef31775c67f0f2ca0fabef3
כרמל צדוק <carmelcarmel88888@gmail.com>
2021-01-03 14:53:59 +0200

    fix(AuthService.js): Fix getAuthUser function

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,22 +1,16 @@
-// import axiosInstance from '../axiosConf';
-// import axios from "axios";
-// import jwt from 'jwt-simple';
-
-import { currentUser } from './MockData';
+import jwt_decode from 'jwt-decode';
 
 class AuthService {
-  // eslint-disable-next-line consistent-return
   static async getAuthUser() {
-    // const { cookie } = document;
-    // if (!cookie) {
-    //   console.log(jwt.decode(document.cookie));
-    document.cookie = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjExMTEiLCJuYW1lIjp7ImZpcnN0TmFtZSI6Iteg15nXmden15kiLCJsYXN0TmFtZSI6IteQ15PXmdeT16EifX0.a6Lpfk8ssaHQtBFxmnyR3iVPL_2hGNuc22670pEa4h8';
-    //   // TODO: Redirect to auth service
-    //   // window.location.replace('http://localhost:3000/');
-    // } else {
-    //   return jwt.decode(cookie);
-    // }
-    return currentUser;
+    const { cookie } = document;
+    if (!cookie) {
+      // TODO: Delete
+      document.cookie = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjExMTEiLCJuYW1lIjp7ImZpcnN0TmFtZSI6Iteg15nXmden15kiLCJsYXN0TmFtZSI6IteQ15PXmdeT16EifX0.a6Lpfk8ssaHQtBFxmnyR3iVPL_2hGNuc22670pEa4h8';
+      // TODO: Redirect to auth service
+      window.location.replace('/');
+    } else {
+      return jwt_decode(document.cookie);
+    }
   }
 }
 
